f58f284e86fb97e722e54d22c1b7fe1d98024e41
will_tech1 <aphilentando55@gmail.com>
2022-04-19 17:39:26 +0200

    First commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
-import { ProductProvider } from "./context";
+import "./style.css"
+
 ReactDOM.render(
-  <ProductProvider>
-    <Router>
-      <App />
-    </Router>
-  </ProductProvider>,
+ <App />,
   document.getElementById("root")
 );
 
